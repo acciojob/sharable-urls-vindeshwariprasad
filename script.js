81c389a19a655fd1cf852baf2ffc8725fc010894
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-04-05 07:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// your code here
+document.addEventListener("DOMContentLoaded", () => {
+  let data = document.getElementById("okay");
+  data.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let names = document.getElementById("name").value;
+    let years = document.getElementById("year").value;
+    let a = "";
+    if (names) {
+      a += "?name=" + names;
+    } if (years && !names) {
+      a += "?year=" + years;
+    } if (names && years) {
+      a += "&year=" + years;
+    }
+    let h = document.getElementById("url");
+    h.innerHTML += a;
+  });
+});
+
